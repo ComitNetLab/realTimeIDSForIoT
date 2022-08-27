a81b1a44449e66531f51a86d76b53d473deb861f
Andres Hernandez <af.hernandezl@uniandes.edu.co>
2022-08-26 23:20:37 -0500

    Socket direct approach, Linux only :c

--- modules/main/main.py
+++ modules/main/main.py
@@ -1,20 +1,40 @@
-import socket
 import platform
 from parsePacket import *
+import sys
 
+# delete later
+import time
+
+print("Init program")
 # Initial config
 system = platform.system()
 if system.startswith('Win'):
+    print("Windows Recognized")
     packetInterface = socket.AF_INET
 elif system.startswith('Lin'):
+    print("Linux recognized")
     packetInterface = socket.PF_PACKET
 else:
     print('Unkown system, exit script')
     sys.exit()
 
-s = socket.socket(packetInterface, socket.SOCK_RAW, socket.IPPROTO_RAW)
+print("Creating socket")
+s = socket.socket(packetInterface, socket.SOCK_RAW, socket.ntohs(3))
+print("Start loop")
 while True:
     raw, add = s.recvfrom(65535)
-    print(ethernetData(s, raw))
-
+    eth = ethernetData(raw)
+    print('Destination: {}, Source: {}, Protocol: {}'.format(eth[0], eth[1], eth[2]))
+    if eth[2] == 8:  # ipv4
+        ipv4 = ipv4Head(eth[3])
+        print('\t - ' + 'IPv4 Packet:')
+        print('\t\t - ' + 'Version: {}, Header Length: {}, TTL: {}, '.format(ipv4[0], ipv4[1], ipv4[2]))
+        print('\t\t - ' + 'Protocol: {}, Source: {}, Target: {}'.format(ipv4[3], ipv4[4], ipv4[5]))
+        '''
+        TCP 6
+        ICMP 1
+        UDP 17
+        RDP 27
+        '''
+    time.sleep(5)
 
--- modules/main/parsePacket.py
+++ modules/main/parsePacket.py
@@ -1,11 +1,35 @@
 import struct
-from struct import *
-import sys
+import socket
+import binascii
 
 
-def ethernetData(socket, raw):
+def ethernetData(raw):
     # 12 bytes for source and dest mac, 2 for EtherType, then data
     dest, src, prototype = struct.unpack('! 6s 6s H', raw[:14])
+    dest = getMacAddr(dest)
+    src = getMacAddr(src)
     proto = socket.htons(prototype)
     data = raw[14:]
     return dest, src, proto, data
+
+
+def ipv4Head(raw):
+    version_header_l = raw[0]
+    version = version_header_l >> 4
+    header_l = (version_header_l & 15) * 4
+    ttl, proto, src, target = struct.unpack('! 8x B B 2x 4s 4s', raw[:20])
+    src = getIp(src)
+    target = getIp(target)
+    data = raw[header_l:]
+    return version, header_l, ttl, proto, src, target, data
+
+
+def getIp(addr):
+    return '.'.join(map(str, addr))
+
+
+def getMacAddr(addr):
+    addr = str(binascii.hexlify(addr))[2:-1]
+    print(addr)
+    print(type(addr))
+    return ':'.join(addr[i:i+2] for i in range(0, len(addr), 2))
