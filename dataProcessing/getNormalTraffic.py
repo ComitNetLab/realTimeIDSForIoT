c0789689e21aa2d2d8c45439be9ca13c909915fd
Andres Hernandez <af.hernandezl@uniandes.edu.co>
2022-08-28 11:53:44 -0500

    initial dataM scripts

---
+++ dataProcessing/getNormalTraffic.py
@@ -0,0 +1,29 @@
+import csv
+
+# Variables
+initPath = 'UNSW_2018_IoT_Botnet_Dataset_'
+fileType = '.csv'
+delimiter = ','
+outputPath = 'IoT_Botnet_Dataset_Normal_Traffic.csv'
+attackPos = 1
+
+
+def load_csv(filename):
+    with open(filename, 'r') as f:
+        reader = csv.reader(f)
+        return list(filter(lambda x: x[attackPos] == 1, list(reader)))
+
+
+with open(outputPath, mode='w') as oFile:
+    writer = csv.writer(oFile)
+
+    print('Init loop')
+    for i in range(1, 75):
+        fileName = initPath + str(i) + fileType
+        print('Loading file ... ' + fileName)
+        data = load_csv(fileName)
+        len(data)
+        writer.writerows(data)
+        print('data filtered and saved.')
+
+    print('File saved in: ', outputPath)
