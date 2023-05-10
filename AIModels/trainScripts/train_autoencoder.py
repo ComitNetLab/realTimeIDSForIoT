a193996dad26e36bcd12aca36180a94bcccf407c
majo <mj.ocampov@uniandes.edu.co>
2023-05-10 18:14:37 -0500

    ajustes a script de entrenamiento de auto-encoders

--- AIModels/trainScripts/train_autoencoder.py
+++ AIModels/trainScripts/train_autoencoder.py
@@ -26,7 +26,6 @@ DATASET = {'normal': PATH + 'IoT_Botnet_Dataset_Normal_Traffic.csv',
            'dos': PATH + 'IoT_Botnet_Dataset_DoS_FULL_Traffic.csv',
            'os_fingerprint': PATH + 'IoT_Botnet_Dataset_OS_Fingerprint_FULL_Traffic.csv',
            'service_scan': PATH + 'IoT_Botnet_Dataset_Service_Scan_FULL_Traffic.csv',
-
            'keylogging': PATH + 'IoT_Botnet_Dataset_Keylogging_FULL_Traffic.csv',
            'keylogging_normal': PATH + 'IoT_Botnet_Dataset_Normal_Keylogging_Traffic.csv',
            'data_exfiltration': PATH + 'IoT_Botnet_Dataset_Data_Exfiltration_FULL_Traffic.csv',
@@ -41,7 +40,7 @@ attack = sys.argv[1].lower()
 features = [f.lower() for f in sys.argv[2:]]
 
 # Load the dataset
-all_files = [DATASET[attack], DATASET[attack + '_normal']]
+all_files = [DATASET[attack], DATASET['normal']]
 cols = ['pkSeqID', 'stime', 'flgs', 'proto', 'saddr', 'sport', 'daddr', 'dport', 'pkts', 'bytes', 'state', 'ltime',
         'seq', 'dur', 'mean', 'stddev', 'smac', 'dmac', 'sum', 'min', 'max', 'soui', 'doui', 'sco', 'dco', 'spkts',
         'dpkts', 'sbytes', 'dbytes', 'rate', 'srate', 'drate', 'attack', 'category', 'subcategory']
@@ -66,36 +65,34 @@ x_trans = enc.fit_transform(x)
 
 # Separate train and test
 x_train, x_test, y_train, y_test = train_test_split(x_trans, y, test_size=0.2, random_state=123, shuffle=True)
+x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.2, random_state=123, shuffle=True)
 
 
 # Define Neural Network
 def train_nn(nn1=512, nn2=128, dropout=0.5, lr=0.001, hidden_activation='relu'):
     nn = Sequential(name='nn_attacks')
-    output = 1
     # Input layer
     nn.add(Input(batch_input_shape=(None, len(features)), name='Capa_Entrada'))
     # Encoder
     nn.add(Dense(nn1, activation=hidden_activation, name='Encoder'))
-    nn.add(Dropout(dropout, name='Dropout_{0}'.format(dropout)))
+    nn.add(Dropout(dropout, name='Dropout_1_{0}'.format(dropout)))
     # Bottleneck
     nn.add(Dense(nn2, activation=hidden_activation, name='Bottleneck'))
     # Decoder
     nn.add(Dense(nn1, activation=hidden_activation, name='Decoder'))
-    nn.add(Dropout(dropout, name='Dropout_{0}'.format(dropout)))
+    nn.add(Dropout(dropout, name='Dropout_2_{0}'.format(dropout)))
     # Ouput layer, softmax activiation
     nn.add(Dense(len(features), activation='tanh', name='Capa_Salida'))
 
     nn.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(learning_rate=lr),
-               metrics=['accuracy'])
+               metrics=['mean_squared_error', 'mae', 'accuracy'])
 
     print(nn.summary())
     return nn
 
 
 # Model used by GridSearch
-# TODO epochs can change
-modelCV = KerasClassifier(build_fn=train_nn, epochs=30, batch_size=200, verbose=1, shuffle=True)
-
+modelCV = KerasClassifier(build_fn=train_nn, verbose=1, shuffle=True)
 
 # Pipeline for transformation and model
 estimators = [('imputer', SimpleImputer(strategy='constant', fill_value=-1)),
@@ -108,13 +105,15 @@ pipeline = Pipeline(estimators)
 param_grid = dict(clf__nn1=[16, 8],
                   clf__nn2=[4, 2],
                   clf__dropout=[0.0, 0.1, 0.2, 0.5],
-                  clf__hidden_activation=['relu', 'sigmoid']
+                  clf__hidden_activation=['relu' 'sigmoid'],
+                  clf__batch_size=[40, 80, 160],
+                  clf__epochs=[10, 20, 50]
                   )
 
 # GridSearch
-grid_search = GridSearchCV(pipeline, param_grid=param_grid, cv=5, verbose=3, scoring='accuracy',
-                                 error_score='raise')
-grid_search.fit(x_train, y_train)
+grid_search = GridSearchCV(pipeline, param_grid=param_grid, cv=5, verbose=1, error_score='raise')
+grid_search.fit(x_train[y_train == 0], x_train[y_train == 0],
+                clf__validation_data=(x_val[y_val == 0], x_val[y_val == 0]))
 
 # Save results of GridSearch
 results = pd.DataFrame(grid_search.cv_results_)
@@ -133,24 +132,34 @@ with open(f'../testResults/model-summary-autoencoder-{attack}.txt', 'a') as f:
 
 # Test metrics
 start = time.time()
-x_pred = loaded_model.predict(x_test)
+x_pred_normal = loaded_model.predict(x_test[y_test == 0])
+x_pred_attack = loaded_model.predict(x_test[y_test == 1])
 end = time.time()
 execution_time = end - start
 # x_pred = [x[0] for x in x_pred]
 
+normal_loss = tf.keras.losses.mse(
+    x_pred_normal, x_test[y_test == 0]
+)
+
+attack_loss = tf.keras.losses.mse(
+    x_pred_attack, x_test[y_test == 1]
+)
 # Calculate MSE
-mse = np.mean(np.power(x_test-x_pred, 2), axis=1)
-error_df = pd.DataFrame({'reconstruction_error': mse,
-                        'true_class': y_test})
+normal_error = pd.DataFrame({'reconstruction_error': normal_loss,
+                             'true_class': y_test[y_test == 0]})
+attack_error = pd.DataFrame({'reconstruction_error': attack_loss,
+                             'true_class': y_test[y_test == 1]})
+error_df = pd.concat([normal_error, attack_error])
 error_df.to_csv(f'../testResults/error-autoencoder-{attack}.csv')
 
 # Visualize error
-threshold_fixed = 50
-groups = error_df.groupby('True_class')
+threshold_fixed = np.mean(normal_loss) + np.std(normal_loss)
+groups = error_df.groupby('true_class')
 fig, ax = plt.subplots()
 
 for name, group in groups:
-    ax.plot(group.index, group.Reconstruction_error, marker='o', ms=3.5, linestyle='',
+    ax.plot(group.index, group.reconstruction_error, marker='o', ms=3.5, linestyle='',
             label="Attack" if name == 1 else "Normal")
 ax.hlines(threshold_fixed, ax.get_xlim()[0], ax.get_xlim()[1], colors="r", zorder=100, label='Threshold')
 ax.legend()
@@ -160,7 +169,7 @@ plt.xlabel("Data point index")
 plt.savefig(f"../testResults/error-autoencoder-{attack}-image.png")
 
 # Evaluate performance from anomaly detection
-threshold_fixed =52
+threshold_fixed = np.mean(normal_loss) + np.std(normal_loss)
 y_pred = [1 if e > threshold_fixed else 0 for e in error_df.reconstruction_error.values]
 error_df['pred'] = y_pred
 
